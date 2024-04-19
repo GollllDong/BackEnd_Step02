4099c7d5a47f44208a34fb7fd157fbc3911b19bb
GollllDong <wjdqjatnwkd@naver.com>
2024-04-19 15:24:44 +0900

    ~ p459 / 페이징 및 Querydsl 연결

--- b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -4,8 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -18,11 +23,11 @@ public class BoardRepositoryTests {
 
     @Test
     public void testInsert() {
-        IntStream.rangeClosed(1,100).forEach(i -> {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
             Board board = Board.builder()
-                    .title("title..."+i)
-                    .content("content..."+i)
-                    .writer("user" + (i%10))
+                    .title("title..." + i)
+                    .content("content..." + i)
+                    .writer("user" + (i % 10))
                     .build();
 
             Board result = boardRepository.save(board); // save에서 sql의 insert, update문을 자동으로 생성해서 실행해준다.
@@ -53,9 +58,9 @@ public class BoardRepositoryTests {
         board.change("--update..title 100--", "--update content 100--");
 
         /*
-        * save : pk가 테이블에 존재하지 않으면 => 삽입 => insert문
-        *        pk 기존 데이터에 존재하면 => 수정 => update문
-        * */
+         * save : pk가 테이블에 존재하지 않으면 => 삽입 => insert문
+         *        pk 기존 데이터에 존재하면 => 수정 => update문
+         * */
         boardRepository.save(board);
     }
 
@@ -64,5 +69,86 @@ public class BoardRepositoryTests {
         Long bno = 1L;
 
         boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPaging() {
+
+        // 페이징할 정보를 생성해서 Pageable 객체에 담아준다.
+        // 첫번째 페이지, 10개씩, bno를 기준으로 정렬하여 역순(최신)으로 가져와라
+        Pageable pageable = PageRequest.of(2, 10, Sort.by("bno").descending());
+        // 페이징에 필요한 정보를 얻어옴
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("total count : " + result.getTotalElements());
+        log.info("total page : " + result.getTotalPages());
+        log.info("total number : " + result.getNumber());
+        log.info("total size : " + result.getSize());
+
+        List<Board> todoList = result.getContent();
+
+        todoList.forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void testTime() {
+        String nowTime = boardRepository.getTime();
+
+        log.info("nowTime : " + nowTime);
+    }
+
+    @Test
+    public void testKeyword() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findKeyword("5", pageable);
+
+        List<Board> todoList = result.getContent();
+
+        todoList.forEach(board -> log.info(board));
+    }
+
+    @Test
+    public void testSearch1() {
+
+        //2 page order by dno desc
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("bno").descending());
+
+        boardRepository.search1(pageable);
+    }
+
+    @Test
+    public void testSearchAll() {
+
+        // title, content, writer
+        String[] types = {"t", "c", "w"};
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
+    }
+
+    @Test
+    public void testSearchAll2() {
+        String[] types = {"t", "c", "w"};
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable);
+
+        // total pages
+        log.info(result.getTotalPages());
+
+        // pag size
+        log.info(result.getSize());
+
+        // pageNumber
+        log.info(result.getNumber());
+
+        // prev next
+        log.info(result.hasPrevious() + ": " + result.hasNext());
+
+        result.getContent().forEach(board -> log.info(board));
     }
 }
