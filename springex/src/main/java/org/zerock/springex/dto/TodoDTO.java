5dfc50273418aa301a9d4a173316a92143de38d7
GollllDong <wjdqjatnwkd@naver.com>
2024-04-21 16:26:38 +0900

    어노테이션 설명 추가

--- springex/src/main/java/org/zerock/springex/dto/TodoDTO.java
+++ springex/src/main/java/org/zerock/springex/dto/TodoDTO.java
@@ -8,17 +8,17 @@ import java.time.LocalDate;
 
 @ToString
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder                // Lombok 어노테이션으로, 빌더 패턴을 구현 -> 객체 생성 시 유연 및 가독성 향상
+@AllArgsConstructor     // 모든 필드를 인자로 받는 생성자들을 자동으로 생성
+@NoArgsConstructor      // 파라미터가 없는 기본 생성자들을 자동으로 생성
 public class TodoDTO {
 
     private Long tno;
 
-    @NotEmpty
+    @NotEmpty               // 해당 필드가 null 이거나 empty 문자열이 아닌지를 검증
     private String title;
 
-    @Future
+    @Future                 // 해당 필드가 현재 시간보다 미래의 시간인지 확인(미래 시간만 허용)
     private LocalDate dueDate;
 
     private boolean finished;
